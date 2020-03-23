4e1d1f67040e5ef7edd7ff233e344333c17b6223
Fanny Rojon <fanny.rojon22@gmail.com>
2020-03-23 22:17:06 +0100

    testing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,9 @@ Rooftop.destroy_all
 
 puts "Start seeding"
 
-Rooftop.create(name: "Beautiful terrace with a view", description: "Beautiful terrace with amazing view on the city", price: 100)
+rooftop_1 = Rooftop.create(name: "Beautiful terrace with a view", description: "Beautiful terrace with amazing view ", price: 100)
+rooftop_2 = Rooftop.create(name: "Gorgeous rooftop with swimming pool", description: "Perfect for pool parties", price: 150)
+rooftop_3 = Rooftop.create(name: "Amazing patio with garden", description: "Incredible patio with luxurious garden", price: 200)
 
 puts "Done seeding"
+puts '*' * 30
